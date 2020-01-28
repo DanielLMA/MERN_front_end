2a7ece366906d2fa1fb35fc12873b4241c856aa3
Ashamaly <ashamalybeard@gmail.com>
2020-01-28 13:34:22 +1000

    gallery and services

--- src/services_page.js
+++ src/services_page.js
@@ -1,11 +1,22 @@
 import React from "react"
 import './style.scss';
+import Header2 from "./header2.js"
+
 
 export default class ServicesPage extends React.Component {
     render() {
         return (
             <>
-            <h1>ServicesPage</h1>
+            <Header2/>
+                <div className="about-container" 
+                style={{backgroundImage: 'url(' + require('./haircut_pic.jpg') + ')'}}
+                >
+                    <div class="content">
+                    <h1>Services</h1>
+                    <p>WAITING FOR INFO FROM CLIENT</p> 
+                    </div>
+
+                </div>
             </>
         )
     }
