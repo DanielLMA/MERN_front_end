b883c5a6da5462c9b254573212e429c8253f0bd0
Ashamaly <ashamalybeard@gmail.com>
2020-01-29 10:39:05 +1000

    css grid location page

--- src/location_page.js
+++ src/location_page.js
@@ -13,9 +13,12 @@ export default class LocationPage extends React.Component {
             
 
             <div class="content-location">
-                <h1>LocationPage</h1> <br />
-                <div><img src={require('./maps.png')} alt="google maps"></img></div>
-                <div>
+                <h1>Location</h1> <br />
+                <div className="contentMaps">
+                    <img src={require('./maps.png')} alt="google maps"></img>
+                </div>
+
+                <div className="contentTimes">
                 <p>
                 RAW BARBERSHOP | SHOP 3 45 STATION ST, NERANG QLD 4211<br />
 
