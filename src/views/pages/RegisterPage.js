d17913b7c09e3415e3789d7678d5d5781267b8c4
Ashamaly <ashamalybeard@gmail.com>
2020-02-06 13:32:57 +1000

    more styling

--- src/views/pages/RegisterPage.js
+++ src/views/pages/RegisterPage.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
 import RegisterForm from "../../components/Forms/fields/RegistrationForm";
 import "./RegisterPage_style.scss"
+import Header from "../../header2"
 
 class RegisterPage extends Component {
     render() {
         // console.log(this.props);
         
         return(
-            <div>
-                <h1>Register a new user</h1>
-                <RegisterForm 
-                history={this.props.history}
-                onRegister={this.props.onRegister} />
+            
+            <div className="contact-body">                        
+            <Header />
+                <div className="contact-content">
+                    <div className="formBackground">
+                        <h1>Register a new user</h1><br />
+                        <RegisterForm 
+                        history={this.props.history}
+                        onRegister={this.props.onRegister} />
+                    </div>
+                </div>
             </div>
         );
     }
